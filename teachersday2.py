a2a48fc777e080fb89eb2739c062b013cd346c5d
pr7523 <parinishtaroy@gmail.com>
2026-09-03 22:31:48 +0530

    Update teachersday2.py

--- teachersday2.py
+++ teachersday2.py
@@ -3,11 +3,13 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: #D63384;'>🌸 Happy Teacher's Day! 🌸</h1>", unsafe_allow_html=True)
 st.markdown("<style>.stApp {background-color: #F4C2C2;}</style>", unsafe_allow_html=True) 
 /* 3. Soft white text input box with a matching border */
+    ''' 
     [data-testid="stTextInputRootElement"] {
         background-color: #FAFAFA !important; 
-        border: 1px solid #8A646A !important;  
+        border: 1.5px solid #8A646A !important;  
         border-radius: 8px;                    
     }
+    '''
 
 
 name = st.text_input("Enter your name:")
