8648be6c2fa3fc27b12d3d15bbfd39e63503075a
pr7523 <parinishtaroy@gmail.com>
2026-09-03 22:26:57 +0530

    Change background color for Teacher's Day app

--- teachersday2.py
+++ teachersday2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.markdown("<h1 style='text-align: center; color: #D63384;'>🌸 Happy Teacher's Day! 🌸</h1>", unsafe_allow_html=True)
-st.markdown("<style>.stApp {background-color: #F9EBEF;}</style>", unsafe_allow_html=True)
+st.markdown("<style>.stApp {background-color: #FDF2F4;}</style>", unsafe_allow_html=True)
 
 
 name = st.text_input("Enter your name:")
