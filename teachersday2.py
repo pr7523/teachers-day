0b3d1144001ab475569451b5c71f17e56168377e
pr7523 <parinishtaroy@gmail.com>
2026-09-03 22:25:49 +0530

    Update teachersday2.py

--- teachersday2.py
+++ teachersday2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.markdown("<h1 style='text-align: center; color: #D63384;'>🌸 Happy Teacher's Day! 🌸</h1>", unsafe_allow_html=True)
-st.markdown("<style>.stApp {background-color: #E66B9E;}</style>", unsafe_allow_html=True)
+st.markdown("<style>.stApp {background-color: #FF8DA1;}</style>", unsafe_allow_html=True)
 
 
 name = st.text_input("Enter your name:")
